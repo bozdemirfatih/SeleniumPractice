f52abc55ba20fda54754cf877e1572fa5a9dfa1b
bozdemirfatih <fthbzdmr@gmail.com>
2023-02-20 00:05:52 +0100

    Practice8

--- src/test/java/Practice/Practice7.java
+++ src/test/java/Practice/Practice7.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.nio.file.WatchEvent;
 import java.time.Duration;
+import java.util.List;
 
 public class Practice7 {
     public static void main(String[] args) {
@@ -33,28 +34,46 @@ public class Practice7 {
         driver.findElement(By.xpath("//span[@class='a-button a-button-primary']")).click();
         WebElement aramakutusuelementi = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
 
-        aramakutusuelementi.sendKeys("apple airpots"+ Keys.ENTER);
+        aramakutusuelementi.sendKeys("apple airpods"+ Keys.ENTER);
 
 
 
         // bulunan sonuc sayisi
       WebElement bulunanSonucSayisi = driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small'][1]"));
 
-        System.out.println( bulunanSonucSayisi);
+        System.out.println( bulunanSonucSayisi.getText());
         //1-16 von 41 Ergebnissen oder Vorschlägen für "apple airpods
         String sonucSayisi=bulunanSonucSayisi.getText();
 
         sonucSayisi=sonucSayisi.substring(
-                ( sonucSayisi.indexOf("von")+3) ,
-                ( sonucSayisi.indexOf("oder")-1)
+                ( sonucSayisi.indexOf("von")+4) ,
+                ( sonucSayisi.indexOf("Ergebnissen")-1)
         );
+
         System.out.println(sonucSayisi);
+        //sonuc sayisinin 100 den fazla oldugunu test edin
+        if (Integer.parseInt(sonucSayisi)>100){
+            System.out.println("sonuc sayisi 100'den fazla test PASSED");
+        } else {
+            System.out.println("sonuc sayisi 100'den fazla degil, test FAILED");
+        }
 
 
 
 
         WebElement ilkurunelementi = driver.findElement(By.xpath("//img[@class='s-image'][1]"));
         ilkurunelementi.click();
+
+        driver.navigate().back();
+        // tum sayfadaki basliklari yazdiralim
+        List <WebElement> baslikElementleriList=driver.findElements(By.xpath("//span[@class='a-size-base a-color-base']"));
+
+        for (WebElement eachelement: baslikElementleriList) {
+            System.out.println(eachelement.getText());
+
+        }
+        driver.close();
+
 
 
 
